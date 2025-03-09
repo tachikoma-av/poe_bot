73eedd3538e7738981448050deb91bd8497f7ac1
White0ps <94152284+Venusonic@users.noreply.github.com>
2025-03-09 21:00:48 +0100

    Add files via upload

---
+++ poe2_simulacrum.bat
@@ -0,0 +1,18 @@
+cd 1_host
+
+set debug=0
+set unique_id=poe2_test
+set build_name=TemporalisBlinker
+set hostname=WIN-POE1
+set remote_ip=192.168.72.253
+
+REM if you want to use the ip, then REM the next line and -- set remote_ip=
+for /f "tokens=2 delims=[]" %%a in ('ping -n 1 %hostname% ^| findstr "["') do set remote_ip=%%a
+
+TITLE %remote_ip% %unique_id% %predefined_strategy% %build_name%
+call venv\Scripts\activate
+
+:infinity_loop
+python poe_2_simulacrum.py {'script':'maps','REMOTE_IP':'%remote_ip%','unique_id':'%unique_id%','force_reset_temp':False,'custom_strategy':'','predefined_strategy':'%predefined_strategy%','build':'%build_name%'}
+REM pause
+goto :infinity_loop
